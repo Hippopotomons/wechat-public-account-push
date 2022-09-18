c1a59ab80edf2bcbbeff823d1b8b2406a758e13c
OiiHo <48543750+Hippopotomons@users.noreply.github.com>
2022-09-18 12:39:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "乖乖",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oy4175tbwq7BEKKxh0TrKVqQy1Q0",
+      id: "oy4175oAHdTUcXRiBxK1zW6c-hSs",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Lu1HqXy20Nk1rftVOT2ZZZpmh50MJwCvjBmS1hRgeyE",
       // 所在省份
@@ -139,7 +139,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "Lu1HqXy20Nk1rftVOT2ZZZpmh50MJwCvjBmS1hRgeyE",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -147,7 +147,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oy4175tbwq7BEKKxh0TrKVqQy1Q0",
     },
     // 你可以不断按格式往下增加
     // ...
